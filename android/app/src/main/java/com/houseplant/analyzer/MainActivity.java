fae4d8138d8465bd077d80617e2f35b9b5a3f421
robot3-track <yohanyinyuchang@gmail.com>
2026-07-22 21:53:06 -0700

    update android build as well as new packages for android APK production local AI

---
+++ android/app/src/main/java/com/houseplant/analyzer/MainActivity.java
@@ -0,0 +1,5 @@
+package com.houseplant.analyzer;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.houseplant.analyzer',
+  appName: 'Houseplant Analyzer',
+  webDir: 'out'
+};
+
+export default config;
